576c2dff23833714152f44d0ed165ade5435000e
Garrett Pauls <garrett.pauls@gmail.com>
2018-06-16 15:53:33 -0600

    Added tests for move checking

--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,10 @@
+#[derive(Debug, PartialEq)]
+pub enum MoveState {
+    Win,
+    Lose,
+    CanMove { vertical: bool, horizontal: bool },
+}
+
 #[derive(Debug)]
 pub struct GameState {
     cells: [u16; 16],
@@ -6,4 +13,74 @@ impl GameState {
     pub fn new() -> GameState {
         GameState { cells: [0; 16] }
     }
+    pub fn check_state(&self) -> MoveState {
+        MoveState::CanMove {
+            vertical: false,
+            horizontal: false,
+        }
+    }
+}
+
+#[test]
+fn check_state_win() {
+    let mut state = GameState::new();
+    state.cells[0] = 2048;
+    assert_eq!(state.check_state(), MoveState::Win);
+}
+
+#[test]
+#[cfg_attr(rustfmt, rustfmt_skip)]
+fn check_state_lose() {
+    let mut state = GameState::new();
+    state.cells =
+        [ 2, 4, 2, 4
+        , 4, 2, 4, 2
+        , 2, 4, 2, 4
+        , 4, 2, 4, 2];
+    assert_eq!(state.check_state(), MoveState::Lose);
+}
+
+#[test]
+#[cfg_attr(rustfmt, rustfmt_skip)]
+fn check_state_in_progress() {
+    let mut state = GameState::new();
+
+    state.cells =
+        [ 2, 4, 2, 4
+        , 2, 8, 4, 2
+        , 2, 4, 2, 4
+        , 4, 2, 4, 2];
+    assert_eq!(
+        state.check_state(),
+        MoveState::CanMove {
+            horizontal: false,
+            vertical: true,
+        }
+    );
+    
+    state.cells =
+        [ 2, 2, 2, 4
+        , 4, 8, 4, 2
+        , 2, 4, 2, 4
+        , 4, 2, 4, 2];
+    assert_eq!(
+        state.check_state(),
+        MoveState::CanMove {
+            horizontal: true,
+            vertical: false,
+        }
+    );
+    
+    state.cells =
+        [ 2, 4, 2, 4
+        , 4, 4, 4, 2
+        , 2, 4, 2, 4
+        , 4, 2, 4, 2];
+    assert_eq!(
+        state.check_state(),
+        MoveState::CanMove {
+            horizontal: true,
+            vertical: true,
+        }
+    );
 }
